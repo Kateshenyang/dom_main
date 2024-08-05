3a50e401bde5fbe974d42bbd53913cbdd26613f3
Ekaterina <kateshenyang@gmail.com>
2024-08-05 21:28:25 +0300

    update

--- ahj-dom-main/README.md
+++
@@ -1,1 +0,0 @@
-[![Build status](https://ci.appveyor.com/api/projects/status/3aqmwj22ci6t4kid/branch/main?svg=true)](https://ci.appveyor.com/project/ArtemKornishon/ahj-dom/branch/main)
--- ahj-dom-main/src/js/startgame.js
+++
@@ -1,9 +0,0 @@
-import Field from './field.js';
-import Goblin from './goblin.js';
-import Game from './game.js';
-
-const field = new Field();
-const goblin = new Goblin();
-const game = new Game(field, goblin);
-
-game.start();
